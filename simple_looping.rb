57d31b9d4cd096cb1609f0a9e65153145d5475b4
Toni Lamont <tlam212@gmail.com>
2018-06-16 21:26:26 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,12 +2,7 @@
 
 def loop_iterator(number_of_times)
   
-  loop do
-   puts "Welcome to Flatiron School's Web Development Course!"
-    
-   if number_of_times >= 8 
-     break
-  end
+  
 end
 
 def times_iterator(number_of_times)
